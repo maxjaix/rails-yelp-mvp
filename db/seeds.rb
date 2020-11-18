126dcb57b4685a9435539a7cc6ab18a16dc96c8f
maxime jourdan <maximejourdan1@gmail.com>
2020-11-18 16:48:57 +0100

    exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Aya = Restaurant.create!(name: 'Aya', address: 'Marseille', category: 'french')
+Papiche = Restaurant.create!(name: 'Papiche', address: 'Nantes', category: 'chinese')
+Hola = Restaurant.create!(name: 'Hola', address: 'Paris', category: 'french')
+Bebe = Restaurant.create!(name: 'Bebe', address: 'Lyon', category: 'italian')
+Flotov = Restaurant.create!(name: 'Flotov', address: 'Marseille', category: 'mexican')
